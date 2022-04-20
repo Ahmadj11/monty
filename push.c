8978c287fc1907ca4c0a84edb56eb7aa56e8923e
Ahmadj11 <95787740+Ahmadj11@users.noreply.github.com>
2022-04-20 14:32:50 -0500

    Create push.c

---
+++ push.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+/**
+ * push - adds a node to the stack
+ * @stack: Pointer to the stack
+ * @line_number: The line count from main
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *newnode = malloc(sizeof(stack_t));
+	int y = atoi(arg);
+
+	if (!newnode)
+	{
+		free(newnode);
+		fprintf(stderr, "Error: malloc failed\n");
+		arg = "error";
+		exit(EXIT_FAILURE);
+	}
+	while (*arg)
+	{
+		if (*arg == '-')
+			arg++;
+		if (isdigit(*arg) == 0)
+		{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		arg = "error";
+		free(newnode);
+		exit(EXIT_FAILURE);
+		}
+		arg++;
+	}
+	newnode->n = y;
+	newnode->next = *stack;
+	newnode->prev = NULL;
+	if (*stack)
+		(*stack)->prev = newnode;
+	*stack = newnode;
+}
